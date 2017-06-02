88ec78891174b6ed50fc6041012ee06a656cdabf
Arjan Dob <arjan1992@hotmail.com>
2017-06-02 12:41:32 +0200

    Changed ping IP, Added comments, Added notice that server is down
    Signed-off-by: Arjan Dob <arjan1992@hotmail.com>

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -1,8 +1,10 @@
+
 <?php
+/*Calling classes for pinigng*/
 	use xPaw\MinecraftPing;
 	use xPaw\MinecraftPingException;
-	
-	define('MQ_SERVER_ADDR', 'play.blockmadness.com');
+	/*server settings to ping */
+	define('MQ_SERVER_ADDR', '5.200.2.204');
 	define('MQ_SERVER_PORT', 25565);
 	define('MQ_TIMEOUT', 1);
 	
@@ -11,7 +13,7 @@
 	
 	$Info = false;
 	$Query = null;
-	
+	/*pinging server*/
 	try {
 		$Query = new MinecraftPing( MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT );
 		$Info = $Query->Query( );
@@ -36,26 +38,30 @@
 		$Query->Close( );
 	}
 ?>
-
+<!-- website -->
 		<!-- Carousel -->
 		<div id="bmCarousel" class="carousel slide" data-ride="carousel">
 			<!-- Overlay logo -->
 			<div class="carousel-overlay">
 				<img class="carousel-overlay-logo" src="img/home-logo01.png">
 			</div>
+			<!-- actual carousel -->
 			<div class="carousel-inner" role="listbox">
+			<!-- slide 1 -->
 				<div class="item active">
 					<img class="first-slide" src="img/home-banner01.png">
 					<div class="container">
 						<div class="carousel-caption"></div>
 					</div>
 				</div>
+				<!-- slide 2 -->
 				<div class="item">
 					<img class="second-slide" src="img/home-banner02.png">
 					<div class="container">
 						<div class="carousel-caption"></div>
 					</div>
 				</div>
+				<!-- slide 3 -->
 				<div class="item">
 					<img class="third-slide" src="img/home-banner03.png">
 					<div class="container">
@@ -63,6 +69,7 @@
 					</div>
 				</div>
 			</div>
+			<!-- Carousel buttons  -->
 			<a class="left carousel-control" href="#bmCarousel" role="button" data-slide="prev">
 				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
@@ -71,17 +78,20 @@
 				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
-		</div><!-- /.carousel -->
+		</div>
+		<!-- end carousel -->
 
 		<!-- Jumbotron -->
 		<div class="jumbotron">
 			<div class="container">
+			<!-- Clickable server IP -->
 				<div class="jumbotron-left">
 					<p>
 						Play with us at <span class="jumbotron-server"><button class="jumbotron-server" data-clipboard-text="play.blockmadness.com">play.blockmadness.com</button></span>
 						<span id="jumbotron-copier"><i class="fa fa-clipboard"></i> Click to copy</span>
 					</p>
 				</div>
+				<!-- Player counter --> 
 				<div class="jumbotron-right">
 					<p><span class="playercount">
 						<?php
@@ -102,6 +112,8 @@
 					</span> players currently online</p></div>
 			</div>
 		</div> <!-- /.jumbotron -->
+		<!-- Notice that server is down -->
+		<div class="alert alert-danger" role="alert">We are aware that the server is not up at this time, We are doing our best to get it working ASAP.</div>
 
 		<!-- Features section -->
 		<div class="features">
@@ -113,6 +125,7 @@
 						<div class="col-md-4 col-md-icon">
 							<i class="fa fa-gamepad fa-3x"></i>
 						</div>
+						<!-- Row one of features -->
 						<div class="col-md-8">
 							<h3>In-game monitoring</h3>
 							<p>Our wonderful staff team is active in all of our servers, in order to provide you the best experience.</p>
@@ -123,8 +136,8 @@
 							<i class="fa fa-gamepad fa-3x"></i>
 						</div>
 						<div class="col-md-8">
-							<h3>Gamemodes</h3>
-							<p>We currently offer Creative, Factions, OP Factions, Survival Games, and Prison servers, with more coming soon!</p>
+							<h3>Games</h3>
+							<p>We don't only offer minecraft server but we also host different game servers! Check outour server page!</p>
 						</div>
 					</div>
 					<div class="col-md-4">
@@ -137,7 +150,8 @@
 						</div>
 					</div>
 				</div>
-
+				<!-- Row 2 of features -->
+				
 				<div class="col-md-8 col-md-offset-2">
 					<div class="col-md-4">
 						<div class="col-md-4 col-md-icon">
@@ -163,7 +177,7 @@
 						</div>
 						<div class="col-md-8">
 							<h3>Ranks</h3>
-							<p>Purchase ranks in our store to receive some cool-perks ingame! Ranks are available for all our gamemodes.</p>
+							<p>We do not Sell any ranks unlikely to other server! With us you rank up depening on your play time!</p>
 						</div>
 					</div>
 				</div>
