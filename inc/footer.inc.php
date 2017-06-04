37f6c7688eb9b62bf241914f719ec6451f06aabf
arriej <arjan1992@hotmail.com>
2017-06-04 23:33:03 +0200

    Added links in footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -9,13 +9,13 @@
 					<a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-arrow-up"></i></a>
 						<ul class="dropdown-menu">
 								<li><a href="#">Need help</a></li>
-								<li><a href="#">Ban Appeal</a></li>
-								<li><a href="#">Report</a></li>
+								<li><a href=https://forums.blockmadness.com/forums/reports-appeals.17/">Ban Appeal</a></li>
+								<li><a href="https://forums.blockmadness.com/forums/reports-appeals.17/">Report</a></li>
 						</ul>
 				</li>
-				<li class="pull-right" style="padding-right:25px;"><a href="#">Staff</a></li>
+				<li class="pull-right" style="padding-right:25px;"><a href="https://forums.blockmadness.com/XenStaff/">Staff</a></li>
 				<li class="pull-right" style="padding-right:25px;"><a href="#">FAQ</a></li>
-				<li class="pull-right" style="padding-right:25px;"><a href="#">ToS</a></li>
+				<li class="pull-right" style="padding-right:25px;"><a href="https://forums.blockmadness.com/wiki/terms-of-service/">ToS</a></li>
 				<div>&copy;&nbsp;BlockMadness&nbsp;<?php echo date("Y"); ?></div>
 			</ul>
 		</div><!-- /Footer -->
